66269064fec4bbd44cfd0e76e2e4308b65066af7
unknown <polovnikovai19i@gmail.com>
2023-09-06 16:21:53 +0300

    исравление3 автотест

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 app.use("/users", require("./routes/users"));
 app.use("/cards", require("./routes/cards"));
 app.use("*", (req,res) => {
-  res.status(404).sendDate({ message: "Страница не найдена" });
+  res.status(404).send({ message: "Страница не найдена" });
 });
 
 app.listen(PORT);
