2448d876f7222b432fe523da44711e774636c5ca
unknown <polovnikovai19i@gmail.com>
2023-09-19 15:55:36 +0300

    автотесты 6

--- routes/signup.js
+++ routes/signup.js
@@ -6,7 +6,7 @@ router.post("/", celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/^(https?:\/\/)?(www\.)?[-a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=,%]+#?$/),
+    avatar: Joi.string().default("https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png").regex(/^(https?:\/\/)?(www\.)?[-a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=,%]+#?$/).message("Поле должно содержать корректную ссылку"),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(5),
   }).unknown(true),
