bce48e9cc34cb0380d7001ad4779898ad95b7ffa
unknown <polovnikovai19i@gmail.com>
2023-09-20 14:48:55 +0300

    tests 2

--- routes/signup.js
+++ routes/signup.js
@@ -10,7 +10,7 @@ router.post(
         name: Joi.string().min(2).max(30),
         about: Joi.string().min(2).max(30),
         avatar: Joi.string().regex(
-          /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=,%]+#?$/
+          /^(https?:\/\/)?(www\.)?[a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=,%-]+#?$/
         ),
         email: Joi.string().required().email(),
         password: Joi.string().required().min(5),
